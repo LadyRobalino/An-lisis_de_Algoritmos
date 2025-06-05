23d9fa85dfc357b6e5d94cfe57852aa19bd9020d
LadyRobalino <lyrobalino1@utpl.edu.ec>
2025-06-05 07:19:43 -0500

    listo

---
+++ TALLERES/Taller_Orden.java
@@ -0,0 +1,72 @@
+// Autor: Lady Robalino
+package TallerAnalisis;
+public class OrdenJava {
+
+    public static void main(String[] args) {
+        int[] array = {4,8,6,2,5,7,1};
+        
+        // Ejemplo de uso
+        int p = 0;
+        int q = (array.length - 1) / 2;
+        int r = array.length - 1;
+
+        merge(array, p, q, r);
+
+        // Imprimir el array resultante
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        
+    }
+    
+    public static void merge(int[] A, int p, int q, int r) {
+        int nL = q - p + 1; 
+        int nR = r - q;     
+
+        int[] L = new int[nL];
+        int[] R = new int[nR];
+
+        // Copiar elementos en L
+        for (int i = 0; i < nL; i++) {
+            L[i] = A[p + i];
+        }
+
+        // Copiar elementos en R
+        for (int j = 0; j < nR; j++) {
+            R[j] = A[q + 1 + j];
+        }
+
+        int i = 0; // índice en L
+        int j = 0; // índice en R
+        int k = p; // índice en A
+
+        // Combinar los arreglos
+        while (i < nL && j < nR) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copiar los elementos restantes de L, si quedan
+        while (i < nL) {
+            A[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copiar los elementos restantes de R, si quedan
+        while (j < nR) {
+            A[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    
+
+}
